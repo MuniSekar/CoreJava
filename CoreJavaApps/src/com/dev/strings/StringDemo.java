29f911ab7047b62490ed341b9a60bf846cfef5df
MuniSekhar Bathala <munisb14@gmail.com>
2015-08-28 12:44:43 +0530

    CoreJava Applications initial commit

---
+++ CoreJavaApps/src/com/dev/strings/StringDemo.java
@@ -0,0 +1,11 @@
+package com.dev.strings;
+
+public class StringDemo {
+	public static void main(String[] args) {
+		String s1="Anil";
+		String s2=new String(s1);
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+				
+	}
+}
